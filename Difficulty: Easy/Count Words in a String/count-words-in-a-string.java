9f39ed1802897b17c666dfbb4df6e9c0096654fb
Srivalli-2007 <sreevallinalli@gmail.com>
2026-09-13 12:17:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Words in a String/count-words-in-a-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countWords(String s) {
+        int count = 0;
+        boolean inWord = false;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                if (!inWord) {
+                    count++;
+                    inWord = true;
+                }
+            } else {
+                inWord = false;
+            }
+        }
+        return count;
+    }
+}
